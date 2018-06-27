7a753350b4277d7d451160b5b11ed56507205e15
masterUNG <protectionsith@hotmail.com>
2018-06-27 13:12:32 +0700

    Initial commit

---
+++ app/src/main/java/rangsan/science4/srru/ac/th/srrufreind/MainActivity.java
@@ -0,0 +1,13 @@
+package rangsan.science4.srru.ac.th.srrufreind;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
